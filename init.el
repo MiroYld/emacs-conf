a3abc1941a1b6ffa4c08a9e6fc66e873fdcf14fc
miro <firat-23@hotmail.fr>
2024-01-10 09:45:54 +0100

    Update config

--- init.el
+++ init.el
@@ -1,4 +1,4 @@
-;; Masquer la barre de menu, la barre d'outils et la scroll bar
+; Masquer la barre de menu, la barre d'outils et la scroll bar
 (menu-bar-mode -1)
 (scroll-bar-mode -1)
 (tool-bar-mode -1)
@@ -6,23 +6,21 @@
 (setq auto-save-default nil
       create-lockfiles nil
       company-idle-delay 0.1
-      company-minimum-prefix-length 1
+      company-minimum-prefix-length 1 
+      doom-modeline-icon nil
+      dracula-alternate-mode-line-and-minibuffer t
       lsp-idle-delay 0.2
       c-set-style "ellemtel"
+      display-line-numbers-type t
       inhibit-splash-screen t
       magit-display-buffer-function #'magit-display-buffer-fullframe-status-v1
       inhibit-startup-message t
-      nyan-animate-nyancat t
       history-length 20
       vc-follow-symlinks t
+      vertico-buffer-mode 1
       make-backup-files nil
       lsp-prefer-capf t
-      ccls-executable "/usr/bin/ccls"
-      company-idle-delay 0
-      custom-file "~/.emacs.d/my-custom.el"
-      ivy-use-virtual-buffers t
-      ivy-count-format "(%d/%d) "
-      )
+      ccls-executable "/usr/bin/ccls")
 
 (defalias 'yes-or-no-p 'y-or-n-p)
 (with-eval-after-load 'elisp-mode
@@ -36,19 +34,39 @@
          counsel-find-file-ignore-regexp "\\.go\\'"	   		 
          enable-recursive-minibuffers t		      		 
          recentf-max-saved-items nil))		      		 
-
 (load-theme 'doom-acario-light t)
 
 (ivy-mode 1)
+(doom-modeline-mode 1)
+(display-battery-mode 1)
+(vertico-mode 1)
+(vertico-posframe-mode 1)
 (counsel-mode)
 (evil-mode)
 (savehist-mode)
-(nyan-mode 1)
 (yas-global-mode 1)
-(solaire-global-mode)
 (global-display-line-numbers-mode)
 (smartparens-global-mode t)
 (toggle-frame-fullscreen)
 
+(with-eval-after-load 'evil
+  (setq evil-want-C-i-jump nil
+        evil-symbol-word-search t
+        evil-insert-state-modes nil
+        evil-motion-state-modes nil
+        evil-move-cursor-back nil
+        evil-kill-on-visual-paste nil
+        evil-move-cursor-back t)
+
+  (mapc (lambda (mode)
+          (evil-set-initial-state mode 'normal))
+        '(fundamental-mode prog-mode text-mode conf-mode diff-mode))
+
+  (fset 'evil-visual-update-x-selection 'ignore)
+
+  (setq-default evil-symbol-word-search t)
+  (defalias 'forward-evil-word 'forward-evil-symbol)
+  
+(add-hook 'edebug-mode-hook 'evil-normalize-keymaps))
 (add-hook 'c-mode-hook 'lsp)
 (add-hook 'c++-mode-hook 'lsp)
